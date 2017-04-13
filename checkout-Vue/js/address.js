21ce77f658f851450f9fe1b8b010617283d83b9b
某人 <243065157@qq.com>
2017-04-13 23:14:22 +0800

    地址选择完成

--- checkout-Vue/js/address.js
+++ checkout-Vue/js/address.js
@@ -1,3 +1,30 @@
 /**
  * Created by 24306 on 2017/4/12.
  */
+new Vue({
+    el: ".container",
+    mounted: function () {
+        this.$nextTick(function () {
+            // 保证加载完成
+            this.getAddressList();
+        })
+    },
+    data: {
+        limitNum:3,
+        addressList: []
+    },
+    computed:{
+        // 默认显示三条
+        fliterAddress:function(){
+            return this.addressList.slice(0,this.limitNum);
+        }
+    },
+    methods: {
+        getAddressList: function () {
+            this.$http.get('data/address.json').then(response => {
+                var res = response.body;
+                this.addressList = res.result;
+            });
+        }
+    }
+})
